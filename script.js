3482146d219a572f32819d3b80bf018e01611935
Dêner Ventura de Oliveira <47732575+Dener-ventura@users.noreply.github.com>
2022-01-17 23:55:22 -0300

    finalizando o jogo

--- script.js
+++ script.js
@@ -48,6 +48,13 @@ function iniciarJogo() {
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game Over :( ")
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
@@ -61,7 +68,13 @@ function iniciarJogo() {
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.X || snakeY != food.Y){
+        snake.pop();
+    }
+    else{food.X = Math.floor(Math.random() * 15 + 1) * box,
+        food.Y = Math.floor(Math.random() * 15 + 1) * box
+
+    }
 
     let newHead = {
         x: snakeX,
